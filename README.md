a3f8338bf14758fb48b3d465e4ebe86f4f18171d
Cristian Sanchez <cristian.manu7@gmail.com>
2023-02-21 09:01:56 -0700

    commit project

---
+++ README.md
@@ -0,0 +1,19 @@
+# ecommerce-store
+
+Front-end and Backend Ecommerce Store
+
+# Details About ecommerce-store
+1. Sass
+2. google fonts
+3. React Router 
+4. useReducers
+5. redux: 
+6. Reselect Library
+7. redux persist
+8. Asynchronous Redux: Redux-Thunk
+9. Saga 
+10. Strip / Serverless Functions / .env
+11. Netlify
+12. TypeScript 
+13. SVG Imports with TypeScript
+14. 
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+
+import App from "./App";
+import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+import "./index.scss";
+
+const rootElement = document.getElementById("root");
+
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  rootElement
+);
+
+serviceWorkerRegistration.register();
